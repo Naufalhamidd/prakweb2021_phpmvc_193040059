98e9cc1905992e8881b2375d6c11a35fadb8f60c
Naufal Hamid <58686931+Naufalhamidd@users.noreply.github.com>
2021-09-30 23:27:08 +0700

    Membuat Model

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -18,7 +18,7 @@ class Mahasiswa_model
 
   public function getAllMahasiswa()
   {
-    $this->stmt = $this->dbh->prepare('SELECET * FROM mahasiswa');
+    $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
     $this->stmt->execute();
     return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
